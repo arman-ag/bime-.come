da8519c726eed0448b86ae3dc9d1fa74cbd576ad
arman-ag <alighanbari.1994@gmail.com>
2024-09-14 06:44:21 +0330

    feat: :sparkles: create show successful message page interface

--- src/app/successful-message/page.tsx
+++ src/app/successful-message/page.tsx
@@ -1,59 +1,17 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { useState } from 'react';
-import { Form, useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
 
 const Home = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const form = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+  const router = useRouter();
+  const backButtonAction = () => {
+    router.push('/');
   };
   return (
     <>
-      <p>آدرس جهت درج روی بیمه‌نامه</p>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className='w-2/3 space-y-6'
-        >
-          <FormField
-            control={form.control}
-            name='username'
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input type='number' placeholder='کدملی' {...field} />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name='username'
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input type='tel' placeholder='شماره تلفن' {...field} />
-                </FormControl>
-
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type='submit'>Submit</Button>
-        </form>
-      </Form>
+      <p>اطلاعات شما با موفقیت ثبت شد.</p>
+      <Button onClick={backButtonAction}>بازگشت</Button>
     </>
   );
 };
